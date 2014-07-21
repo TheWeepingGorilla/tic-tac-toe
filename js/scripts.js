35613176889f16b59ae1f1cc4c9e9de8eb9eea6f
SeanMorrison112358@gmail.com <student@epicodus.com>
2014-07-20 17:38:43 -0700

    Test HTML begun

--- js/scripts.js
+++ js/scripts.js
@@ -136,51 +136,53 @@ var Game = {
 }
 
 
-// $(document).ready(function() {
-//   $('#new-tamagotchi-form').submit(function(event) {
-//     var newTamagotchi = Object.create(Tamagotchi);
+$(document).ready(function() {
+  $('#new-game').click(function(event) {
+    var newGame = Object.create(Game);
+    newGame.initialize();
+    alert("Game initialized!");
 
-//     newTamagotchi.initialize($('input#new-tamagotchi-name').val());
-//     $('#food').append(newTamagotchi.foodLevel);
-//     $('#activity').append(newTamagotchi.activityLevel);
-//     $('#sleep').append(newTamagotchi.sleepLevel);
-//     event.preventDefault();
+    // newTamagotchi.initialize($('input#new-tamagotchi-name').val());
+    // $('#food').append(newTamagotchi.foodLevel);
+    // $('#activity').append(newTamagotchi.activityLevel);
+    // $('#sleep').append(newTamagotchi.sleepLevel);
+    // event.preventDefault();
 
-//     $('#hourglass').click(function(event) {
-//       newTamagotchi.hungry();
-//       newTamagotchi.sleepy();
-//       newTamagotchi.bored();
-//       if (!newTamagotchi.isAlive()) {
-//         alert("Game Over man!  Game over!");
-//       }
-//       $('#food').empty();
-//       $('#food').append(newTamagotchi.foodLevel);
-//       $('#activity').empty();
-//       $('#activity').append(newTamagotchi.activityLevel);
-//       $('#sleep').empty();
-//       $('#sleep').append(newTamagotchi.sleepLevel);
-//     });
+    // $('#hourglass').click(function(event) {
+    //   newTamagotchi.hungry();
+    //   newTamagotchi.sleepy();
+    //   newTamagotchi.bored();
+    //   if (!newTamagotchi.isAlive()) {
+    //     alert("Game Over man!  Game over!");
+    //   }
+    //   $('#food').empty();
+    //   $('#food').append(newTamagotchi.foodLevel);
+    //   $('#activity').empty();
+    //   $('#activity').append(newTamagotchi.activityLevel);
+    //   $('#sleep').empty();
+    //   $('#sleep').append(newTamagotchi.sleepLevel);
+    // });
 
-//     $('#foodbtn').click(function(event) {
-//       newTamagotchi.fed();
-//       $('#food').empty();
-//       $('#food').append(newTamagotchi.foodLevel);
-//     });
+    // $('#foodbtn').click(function(event) {
+    //   newTamagotchi.fed();
+    //   $('#food').empty();
+    //   $('#food').append(newTamagotchi.foodLevel);
+    // });
 
-//     $('#playbtn').click(function(event) {
-//       newTamagotchi.recess();
-//       $('#activity').empty();
-//       $('#activity').append(newTamagotchi.activityLevel);
-//     });
+    // $('#playbtn').click(function(event) {
+    //   newTamagotchi.recess();
+    //   $('#activity').empty();
+    //   $('#activity').append(newTamagotchi.activityLevel);
+    // });
 
-//     $('#sleepbtn').click(function(event) {
-//       newTamagotchi.sleepyTime();
-//       $('#sleep').empty();
-//       $('#sleep').append(newTamagotchi.sleepLevel);
-//     });
+    // $('#sleepbtn').click(function(event) {
+    //   newTamagotchi.sleepyTime();
+    //   $('#sleep').empty();
+    //   $('#sleep').append(newTamagotchi.sleepLevel);
+    // });
 
-//   });
-// });
+  });
+});
 
 
 
